3ddb6b67cc50a67b38b8db76a04bbd1924925677
Camila Rodriguez <rodriguezcaami111@gmail.com>
2026-03-27 14:23:50 -0300

    Agrego ejemplos de modulos

---
+++ events-ejemplo/index.mjs
@@ -0,0 +1,10 @@
+import { EventEmitter } from 'events'; 
+// Crear una instancia de EventEmitter 
+const emisor = new EventEmitter(); 
+// Definir un evento personalizado 
+emisor.on('saludo', (nombre) => { 
+console.log(`¡Hola, ${nombre}!`); 
+}); 
+
+// Emitir el evento 'saludo' 
+emisor.emit ('saludo', 'Mundo'); 
